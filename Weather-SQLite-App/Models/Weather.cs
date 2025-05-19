2dd3fa68be8db6f9557382d25049a83fb5cc52df
231008 <231008@CPD01-M19>
2025-05-19 11:03:11 -0300

    Weather model

---
+++ Weather-SQLite-App/Models/Weather.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SQLite;
+
+namespace Weather_SQLite_App.Models
+{
+    public class Weather
+    {
+        [PrimaryKey, AutoIncrement]
+        public int Id { get; set; }
+        public double? Longitude { get; set; }
+        public double? Latitude { get; set; }
+        public int? Visibility { get; set; }
+        public double? MaxTemperature { get; set; }
+        public double? MinTemperature { get; set; }
+        public double? Temperature { get; set; }
+        public string? Sunrise { get; set; }
+        public string? Sunset { get; set; }
+        public string? Main { get; set; }
+        public string? Description { get; set; }
+        public double? WindSpeed { get; set; }
+    }
+}
